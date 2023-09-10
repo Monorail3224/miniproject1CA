d4cc27173d9eefdbc783d16c24573682b6991e16
Monorail3224 <c_allen12@mail.fhsu.edu>
2023-09-10 16:01:11 -0500

    Modified parameter period and created config.py file as staging area for my API key

--- miniproject1CA.py
+++ miniproject1CA.py
@@ -1,3 +1,30 @@
 # INF601 - Advanced Programming in Python
 # Clayton Allen
 # Mini Project 1
+
+"""
+This script makes use of a REST API available through rapidapi.com. 
+Please visit the provided link to explore additional endpoints.
+https://rapidapi.com/asepscareer/api/yfinance-stock-market-data/
+"""
+
+import requests
+from config import *
+import pprint
+
+url = "https://yfinance-stock-market-data.p.rapidapi.com/price"
+
+payload = {
+	"period": "10d",
+	"symbol": "AAPL"
+}
+headers = {
+	"content-type": "application/x-www-form-urlencoded",
+	"X-RapidAPI-Key": X_RapidAPI_Key, # Assigned variable in "config.py" that contains the API key
+	"X-RapidAPI-Host": "yfinance-stock-market-data.p.rapidapi.com"
+}
+
+response = requests.post(url, data=payload, headers=headers)
+
+pprint.pprint(response.json())
+    
