264a0b04bf883fcd948608388f5ddbbd9ecd2eb4
Monorail3224 <c_allen12@mail.fhsu.edu>
2023-09-11 19:01:39 -0500

    Added required Libraries and modules. Converted script into a function and added comments. Added timer to API calls

--- miniproject1CA.py
+++ miniproject1CA.py
@@ -2,29 +2,40 @@
 # Clayton Allen
 # Mini Project 1
 
+#The required libraries and modules for this project to work. List of all dependencies will be shown in the "Requirements.txt" file located in 
+
 """
 This script makes use of a REST API available through rapidapi.com. 
 Please visit the provided link to explore additional endpoints.
 https://rapidapi.com/asepscareer/api/yfinance-stock-market-data/
 """
 
-import requests
-from config import *
-import pprint
+import os # Required to make the directory "Images" which is used as a staging area for outputted image files. 
+import requests  # Required to send API requests to endpoints
+import matplotlib.pyplot as plt # This is used to output data from API responses to scatterplot PNG files. 
+from config import * # This imports everything located in the "Config.py" file, which is used as a placeholder for your API key. 
+from datetime import datetime   # Used to convert epoch time to standard datetime. 
+import numpy as np  # Used to convert the list of data into a numpy array, later to be used as input data for the scatterplot images.
+import time  # Import the time module
 
-url = "https://yfinance-stock-market-data.p.rapidapi.com/price"
+# Function used to get stock data over the past 10 days for a given stock symbol.
+def get_trades(apikey, symbol): #Positional arguments used as input for funciton. 
+    url = "https://yfinance-stock-market-data.p.rapidapi.com/price"
 
-payload = {
-	"period": "10d",
-	"symbol": "AAPL"
-}
-headers = {
-	"content-type": "application/x-www-form-urlencoded",
-	"X-RapidAPI-Key": X_RapidAPI_Key, # Assigned variable in "config.py" that contains the API key
-	"X-RapidAPI-Host": "yfinance-stock-market-data.p.rapidapi.com"
-}
+    # Feel free to modify the day count. 
+    payload = {
+        "period": "10d",
+        "symbol": symbol
+    }
+    headers = {
+        "content-type": "application/x-www-form-urlencoded",
+        "X-RapidAPI-Key": apikey,
+        "X-RapidAPI-Host": "yfinance-stock-market-data.p.rapidapi.com"
+    }
 
-response = requests.post(url, data=payload, headers=headers)
+    # Adjust this as needed. RapidAPI "yFinance" only allows for one query per second for free users. You can reduce this wait value by giving the yfinance overlords more money.  
 
-pprint.pprint(response.json())
+    time.sleep(1)
+
+    response = requests.post(url, data=payload, headers=headers)
     
