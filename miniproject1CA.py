0c3890d51eeae340502e9d314039ba40a43ffcef
Monorail3224 <c_allen12@mail.fhsu.edu>
2023-09-16 12:56:01 -0500

    Utilized date module to convert epoch time to standard date-time. Added the close price to lists and converted to numbpy array

--- miniproject1CA.py
+++ miniproject1CA.py
@@ -47,7 +47,6 @@ def get_trades(apikey, symbol): #Positional arguments used as input for funciton
     else:
         print(f"Error: {response.status_code}")
         return None
-    
     # List of symbols for the companies that I've invested in the past.
 companies = ["AAPL", "GE", "NET", "NVDA", "RTX"]
 
@@ -58,3 +57,17 @@ if not os.path.exists(output_directory):
 
 for symbol in companies:
     data = get_trades(apikey=X_RapidAPI_Key, symbol=symbol)
+
+if data is not None:
+        # Extract the close values and dates from the API response and save them to lists
+        close_prices = [entry['Close'] for entry in data]
+        # Convert Epoch time to standard datetime
+        dates = [datetime.utcfromtimestamp(entry['Date'] / 1000).strftime('%Y-%m-%d') for entry in data]
+
+        # Convert the lists to NumPy ndarrays
+        close_prices_array = np.array(close_prices)
+        dates_array = np.array(dates)
+
+        # The creation of this directory is relative to the path in which the python script is run from.
+        # Define the file path where you want to save the image for each company and append "_close_prices.png" to the end of the stock symbol
+        file_path = os.path.join(output_directory, f'{symbol.lower()}_close_prices.png')
